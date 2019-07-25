b0b6e5065d39cdc7c5b9f3ebcf411862a9298993
陈海楠 <1806632927chen@gmail.com>
2019-07-25 18:05:58 +0800

    链接

---
+++ src/main/kotlin/com/chen/bing/picture/constants/PictureConstants.kt
@@ -0,0 +1,14 @@
+package com.chen.bing.picture.constants
+
+/**
+ * @author: 1806632927@qq.com
+ * @date: 2019/7/25
+ * @description:
+ */
+object PictureConstants {
+
+    val baseUrL = "https://cn.bing.com/HPImageArchive.aspx?"
+
+    val preUrl = "https://cn.bing.com"
+
+}
